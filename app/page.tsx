952cf2127fcac8369b4d1b92b355a5b094b2ebe4
Bryan Yung <byung806@gmail.com>
2025-01-28 00:59:59 -0500

    1 row

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 export default function Home() {
+  const letters = ['C', 'L', 'O', 'U', 'D'];
+
   return (
     <div className="flex justify-center h-screen items-center">
       {/* The container holding all the boxes */}
-      <div className="h-10 w-10 bg-slate-400 p-4">
+      <div className="flex space-x-1">
+
+        {/* 1 row of boxes */}
+        {letters.map((letter, index) => (
+          <div key={index} className="flex text-2xl font-bold text-white bg-slate-500 h-10 w-10 p-4 items-center justify-center">
+            {letter}
+          </div>
+        ))}
       </div>
     </div>
   );
