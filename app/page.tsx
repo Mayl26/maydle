6722ea599cbef182daaec39544a484d1a8057c92
May Liu <Mayshunliu@gmail.com>
2025-01-27 23:40:37 -0700

    Added row component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
+import Row from "@/components/row";
+
 export default function Home() {
   const letters = ['C', 'L', 'O', 'U', 'D'];
+  const numberofrows = 6;
 
   return (
     <div className="flex justify-center h-screen items-center">
       {/* The container holding all the boxes */}
-      <div className="flex space-x-1">
-
-        {/* 1 row of boxes */}
-        {letters.map((letter, index) => (
-          <div key={index} className="flex text-2xl font-bold text-white bg-slate-500 h-10 w-10 p-4 items-center justify-center">
-            {letter}
-          </div>
+      <div className="flex-col space-y-1 ">
+        {/* Making 6 columns of 5 rows */}
+        {[...Array(numberofrows)].map((_, index) => (
+          <Row 
+            key={index} 
+            letters={letters}
+          /> 
         ))}
       </div>
+
     </div>
   );
 }
