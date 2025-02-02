726a0a044cace70c2c31e9126154076bc0ffd2db
Bryan Yung <byung806@gmail.com>
2025-02-02 01:51:39 -0500

    start logic

---
+++ logic/wordle.ts
@@ -0,0 +1,12 @@
+// 1) verify the word is actually in valid words
+// 2) check which letters are yellow and which are green
+
+// check word is valid word in the list
+function isValidWord(word: string): boolean {
+
+}
+
+// returns the indices of yellow letters and the indices of green letters
+function checkLetters(word: string, answer: string): { yellow: number[], green: number[] } {
+    
+}
